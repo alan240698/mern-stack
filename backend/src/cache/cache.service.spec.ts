356eb6b7de432a82493daba30fe27a66a31af35f
luonglt-kimei <luonglt@kimei.vn>
2025-03-20 14:46:22 +0700

    Update deploy fix docker-compose not exist ver1

--- backend/src/cache/cache.service.spec.ts
+++ backend/src/cache/cache.service.spec.ts
@@ -1,18 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CacheService } from './cache.service';
+import { CacheModule } from '@nestjs/cache-manager';  // Import CacheModule từ NestJS Cache
 
 describe('CacheService', () => {
   let service: CacheService;
 
   beforeEach(async () => {
+    // Import CacheModule vào TestModule để cung cấp CACHE_MANAGER
     const module: TestingModule = await Test.createTestingModule({
+      imports: [CacheModule],  // Import CacheModule để cung cấp CACHE_MANAGER
       providers: [CacheService],
     }).compile();
 
-    service = module.get<CacheService>(CacheService);
+    service = module.get<CacheService>(CacheService);  // Get CacheService
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(service).toBeDefined();  // Kiểm tra CacheService đã được khởi tạo đúng
   });
 });
